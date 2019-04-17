6ad83f3c950fa50497f1007f71df9c14866c67e1
william seo <williamseo1234@gmail.com>
2019-04-17 12:59:11 -0700

    Fixes more

--- functions.js
+++ functions.js
@@ -2,22 +2,54 @@ const request = require('request');
 
 var getImages = (query, callback) => {
     request({
-        url: '"https://images-api.nasa.gov/search?q=' + encodeURIComponent(query),
+        url: 'https://images-api.nasa.gov/search?q=' + encodeURIComponent('Earth'),
         json: true
     }, (error, response, body) => {
         if (error) {
-            callback('Cannot connect to rest-countries API');
+            callback('Cannot connect to Nasa API');
         }
-        else if (body.status === 'ZERO_RESULTS') {
-            callback('Cannot find requested country');
+        else if (body.collection.metadata.total_hits === 0) {
+            callback('Cannot find requested images');
         }
-        else if (body.status !== 'ZERO_RESULTS') {
-            callback(undefined, {
-                image1: collection.items[0].links.href,
-                image2: collection.items[1].links.href,
-                image3: collection.items[2].links.href,
-                image4: collection.items[3].links.href
-            })
+        else {
+            for (images in body.collection.items) {
+                var x = document.createElement("IMG");
+                x.setAttribute("src", "body.collection.items[images].links[0].href");
+                x.setAttribute("width", "304");
+                x.setAttribute("height", "228");
+                x.setAttribute("alt", "The Pulpit Rock");
+                document.body.appendChild(x);
+            }
         }
     });
+};
+
+var getCards = (numOfCards, callback) => {
+    console.log('Loading Cards');
+    request({
+        url: 'https://deckofcardsapi.com/api/deck/new/draw/?count=' + encodeURIComponent(3),
+        json: true
+    }, (error, response, body) => {
+        if (error) {
+            callback('Cannot connect to Cards API');
+        }
+        else if (len(body.cards) === 0) {
+            callback('Cannot request 0 cards');
+        }
+        else {
+            for (card in body.cards) {
+                var x = document.createElement("IMG");
+                x.setAttribute("src", "body.cards[card].image)");
+                x.setAttribute("width", "304");
+                x.setAttribute("height", "228");
+                x.setAttribute("alt", "The Pulpit Rock");
+                document.body.appendChild(x);
+            }
+        }
+    });
+};
+
+module.exports = {
+    getImages,
+    getCards
 };
--- server.js
+++ server.js
@@ -12,18 +12,22 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
+    console.log('User requested welcome page');
     response.render('welcome.hbs');
 });
 
 app.get('/nasa', (request, response) => {
+    console.log('User requested nasa page');
     response.render('nasa.hbs');
 });
 
 app.get('/cards', (request, response) => {
+    console.log('User requested cards page');
     response.render('cards.hbs');
 });
 
 app.get('/404', (request, response) => {
+    console.log('User requested error page');
     response.send({
         error: 'Page not found'
     })
